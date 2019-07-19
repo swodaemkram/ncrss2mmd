9274964cab81ca4b9891e946b733c1a454115c86
mark <mark@mark-NUC7i5BNK>
2019-07-19 11:11:25 -0400

    Second Commit

--- src/ncrss2mmd.cpp
+++ src/ncrss2mmd.cpp
@@ -1,15 +1,63 @@
 //============================================================================
 // Name        : ncrss2mmd.cpp
 // Author      : Mark Meadows
-// Version     : ver 0.0.1
-// Copyright   : Copyright (c) 2019 Mark MEadows
-// Description : Hello World in C++, Ansi-style
+// Version     : 0.0.1
+// Copyright   : Copyright (c) 2019 Mark Meadows
+// Description :nc2ss2md in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include "tinyxml/tinyxml.h"
+
+
+
 using namespace std;
 
-int main() {
-	cout << "ncrss2mmd" << endl; // prints ncrss2mmd
-	return 0;
+int main()
+{
+
+		char test[250000];
+	    TiXmlDocument doc("Test.xml");
+		doc.LoadFile();
+
+// *******    We have opened the XML file and are ready to run through it   **********
+
+		      TiXmlElement *root, *pelem, *elem ;
+
+		       root = doc.FirstChildElement();
+
+		       //for(elem = root->FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement())
+
+
+		       for (elem = root->FirstChildElement("channel"); elem != NULL; elem = elem->NextSiblingElement())
+
+		       {
+
+		    	  	 // pelem = elem->FirstChildElement("title");
+
+		    	   	   pelem =elem->FirstChildElement("title");
+		    	       if (pelem) strcpy(test, (char*) pelem->GetText());
+		    		   printf("The Data 1 = %s\n",test);
+
+		    		   pelem = elem->FirstChildElement();
+		    		   if (pelem) strcpy(test, (char*) pelem->GetText());
+		    		   printf("The Data 2 = %s\n",test);
+
+		       }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
