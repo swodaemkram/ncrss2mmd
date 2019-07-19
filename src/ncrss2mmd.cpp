1b96ba5a599243d102bcd4cf2be5f1c0cb5de081
mark <mark@mark-NUC7i5BNK>
2019-07-19 14:33:41 -0400

    Put XML Parsing in its own function

--- src/ncrss2mmd.cpp
+++ src/ncrss2mmd.cpp
@@ -10,49 +10,57 @@
 #include <string>
 #include "tinyxml/tinyxml.h"
 
-
-
 using namespace std;
+
+void parse_xml(void); //Function to parse RSS feed from NextCloud
 
 int main()
 {
 
-	char test[250000];
-	TiXmlElement *pelem;
-
-
-
-	TiXmlDocument   doc("Test.xml");
-
-	    if (doc.LoadFile())
-	    {
-	        TiXmlNode *elem = doc.FirstChildElement()->FirstChildElement()->FirstChildElement("item");
-	        pelem =elem->FirstChildElement("title");
-	        if (pelem) strcpy(test, (char*) pelem->GetText());
-	        printf(" %s\n",test);
-
-	        pelem =elem->FirstChildElement("pubDate");
-	        if (pelem) strcpy(test, (char*) pelem->GetText());
-	        printf(" %s\n",test);
-
-
-
-	    }
-
-
-
-
-
-
-
-
+parse_xml(); //Parse XML from NextCloud
 
 
 }
 
 
+/*
+========================================================================================
+Function To Parse XML From NextCloud
+All we need is the last piece of information published
+========================================================================================
+ */
 
 
+void parse_xml(void)
+{
+	char test[250000];
+		TiXmlElement *pelem;
+
+
+
+		TiXmlDocument   doc("Test.xml");
+
+		    if (doc.LoadFile())
+		    {
+		        TiXmlNode *elem = doc.FirstChildElement()->FirstChildElement()->FirstChildElement("item");
+		        pelem =elem->FirstChildElement("title");
+		        if (pelem) strcpy(test, (char*) pelem->GetText());
+		        printf(" %s\n",test);
+
+		        pelem =elem->FirstChildElement("pubDate");
+		        if (pelem) strcpy(test, (char*) pelem->GetText());
+		        printf(" %s\n",test);
+
+		    }
+
+return;
+
+}
+/*
+=============================================================================================
+End of XML Parsing Function
+=============================================================================================
+ */
 
 
 
