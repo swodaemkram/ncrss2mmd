f6c4dafc9663b080d51249bfd774b8ff6fd595cd
mark <mark@mark-NUC7i5BNK>
2019-07-19 11:05:39 -0400

    test

---
+++ src/ncrss2mmd.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ncrss2mmd.cpp
+// Author      : Mark Meadows
+// Version     :
+// Copyright   : Copyright (c) 2019 Mark MEadows
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "ncrss2mmd" << endl; // prints ncrss2mmd
+	return 0;
+}
