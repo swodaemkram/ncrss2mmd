f6a971df5a5000dca499775a94bd02e413b2a20f
mark <mark@mark-desktop>
2019-07-20 13:27:13 -0400

    Reading the config file is complete and tested

--- src/ncrss2mmd.cpp
+++ src/ncrss2mmd.cpp
@@ -17,10 +17,20 @@ void get_nextcloud_rssfeed(void);
 void send_data_to_mattermost(void);
 void read_config(void);
 
+char RssURL[250] = {};       //A place to put the Rss Feeds URL
+char WebHookURL[250] = {};	 //A Place to put the WebHookURL
+char Filter[250] = {};       //A Place for the Filter Info
+
 int main()
 {
 
 read_config();
+
+printf("RssURL = %s\n",RssURL);         //Debug
+printf("WebHookURL = %s\n",WebHookURL); //Debug
+printf("Filter = %s\n",Filter);			//Debug
+
+
 get_nextcloud_rssfeed();    //Download the Rss Feed from NextCloud and hand it off to the parser
 parse_xml();                //Parse RSS Feed XML from NextCloud (completed)
 send_data_to_mattermost();  //Send the gleaned data to MatterMost Server Via Web Hook
@@ -70,8 +80,33 @@ void send_data_to_mattermost(void)
 	return;
 }
 
+/*
+============================================================================================
+Lets read the Config File and Load it
+============================================================================================
+ */
+
 void read_config(void)
 {
+	//With only a few lines in the config file we will just make a conf "text" file
+	//We will put it in the normal place /etc and we will call it ncrssmmd.conf
+	FILE *Config_File = NULL;                        // declare config file Pointer
+
+		 		Config_File = fopen("/etc/ncrss2mmd.conf", "r");  	// Open config file
+		 		if (Config_File == NULL){
+		 			printf("Could not open Config File\n");
+		 			exit(1);
+		 		}
+
+		 		fscanf(Config_File,"%s", RssURL);
+		 		fscanf(Config_File,"%s", WebHookURL);
+		 		fscanf(Config_File,"%s", Filter);
+		 		fclose(Config_File);
+
 	return;
 }
-
+/*
+============================================================================================
+Config File Loaded
+============================================================================================
+*/
